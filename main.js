a479e2423ac279cf21e6e3d5888e792ca21094f6
Pliv <y.dekker02@student.alfa-college.nl>
2023-02-14 15:18:03 +0100

    cookie popup

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let count = 0;
+let accepted = "accepted; expires=Thu, 01 Jan 2300 00:00:00 UTC; path=/; SameSite=Lax;";
+let acceptButton = document.querySelector('.accept');
+let denyButton = document.querySelector('.deny');
+let cookieElement = document.querySelector('#cookie');
+let myWindow;
 
-window.addEventListener("scroll", (event) => {
+window.addEventListener("scroll", (event) => { // checks if user is scrolling and sets the view back a few pixels
     let scroll = this.scrollY;
     count++
     console.log(count)
@@ -10,3 +15,25 @@ window.addEventListener("scroll", (event) => {
     count = 0;
 }
 });
+
+acceptButton.addEventListener("click", hideCookie); // simple event listener
+denyButton.addEventListener("click", closeWin); // simple event listener
+
+function closeWin() {
+    while (true) {
+        
+    }
+}
+
+function hideCookie() {
+    cookieElement.style.display = "none"; // hides popup
+    document.cookie = accepted; // makes a cookie
+}
+
+if (document.cookie == "accepted") { // checkes if cookie is already accepted
+    hideCookie();
+}
+
+function clearStorage() { // clears cookies used for debugging mostly
+    document.cookie = null;
+}
